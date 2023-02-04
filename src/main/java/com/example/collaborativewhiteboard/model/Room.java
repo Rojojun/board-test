ca5a14196cc9328eb370e8b7076bbb20646a7e50
Hojun Na <72805486+Rojojun@users.noreply.github.com>
2023-02-04 22:12:39 +0900

    dependency 관련 재수정 NoSQL에 의한 gradle 변경

--- src/main/java/com/example/collaborativewhiteboard/model/Room.java
+++ src/main/java/com/example/collaborativewhiteboard/model/Room.java
@@ -1,10 +1,10 @@
 package com.example.collaborativewhiteboard.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import javax.persistence.Id;
 import java.util.List;
 
 @Data
